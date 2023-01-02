ba0b5ffd3be17a672f5949306017bbcf3d798754
dlopez1591 <83315962+dlopez1591@users.noreply.github.com>
2023-01-02 08:55:17 -0500

    task 2 mejorada

--- Task 1.2/main.js
+++ Task 1.2/main.js
@@ -1,16 +1,26 @@
-let cards = document.getElementById("cards");
-function crearCard (array, where){
-    let card = document.createElement("div")
-    for (let cardSmall of array){
-        card.innerHTML += ` <section class="card m-2 d-flex flex-column justify-content-center align-items-center" style="width: 18rem; height: 22rem;">
-        <img src=${cardSmall.image} class="card-img-top m-3 " style="width: 220px; height: 130px;" alt="...">
-        <section class="card-body d-flex flex-column justify-content-center align-items-center">
-          <h5 class="card-title">${cardSmall.name}</h5>
-          <p class="card-text" style="text-align: justify;">${cardSmall.description}.</p>
-          <a href="#" class="btn btn-primary" style="background-color: #D90368;">Go somewhere</a>
-        </section>`
-
-    }
-    where.append(card)
-}
-crearCard(data, cards)
+function upcomingEvents (data){
+  let events = []
+  for (let event of data.events){
+          events.push(event);
+      }
+      return events  
+  }
+  
+  
+  
+  let cards = document.getElementById("cards");
+  function crearCard (array, where){
+      let card = document.createElement("div")
+      for (let cardSmall of array){
+          card.innerHTML += ` <div class="card m-2 d-flex flex-column justify-content-center align-items-center" style="width: 18rem; height: 22rem;">
+          <img src=${cardSmall.image} class="card-img-top m-3 " style="width: 220px; height: 130px;" alt="...">
+          <div class="card-body d-flex flex-column justify-content-center align-items-center">
+            <h5 class="card-title">${cardSmall.name}</h5>
+            <p class="card-text" style="text-align: justify;">${cardSmall.description}.</p>
+            <a href="./events.html" class="btn btn-primary" style="background-color: #D90368;">View More</a>
+          </div>`
+  
+      }
+      where.append(card)
+  }
+  crearCard(upcomingEvents (data), cards)
---
+++ Task 1.2/upcoming.js
@@ -0,0 +1,29 @@
+function upcomingEvents (data){
+let upcomingEvents = []
+for (let event of data.events){
+    let currentDate = new Date (data.currentDate)
+    let dateEvent = new Date (event.date)
+    if(currentDate < dateEvent){
+        upcomingEvents.push(event);
+    }
+}
+return upcomingEvents
+}
+
+
+let cards = document.getElementById("cards");
+function crearCard (array, where){
+    let card = document.createElement("div")
+    for (let cardSmall of array){
+        card.innerHTML += ` <div class="card m-2 d-flex flex-column justify-content-center align-items-center" style="width: 18rem; height: 22rem;">
+        <img src=${cardSmall.image} class="card-img-top m-3 " style="width: 220px; height: 130px;" alt="...">
+        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+          <h5 class="card-title">${cardSmall.name}</h5>
+          <p class="card-text" style="text-align: justify;">${cardSmall.description}.</p>
+          <a href="./events.html" class="btn btn-primary" style="background-color: #D90368;">View More</a>
+        </div>`
+
+    }
+    where.append(card)
+}
+crearCard(upcomingEvents (data), cards)
